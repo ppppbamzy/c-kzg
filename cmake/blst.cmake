6f88c24038df1c82b1cc636d0a5fec4fb67e2cc4
ppppbamzy <pppp.bamzy@gmail.com>
2023-02-06 18:58:19 +0800

    use cmake

---
+++ cmake/blst.cmake
@@ -0,0 +1,20 @@
+include(FetchContent)
+
+FetchContent_Declare(blst
+    GIT_REPOSITORY https://github.com/supranational/blst
+)
+
+
+# FetchContent_MakeAvailable(blst)
+FetchContent_GetProperties(blst)
+if(NOT miracl_POPULATED)
+    FetchContent_Populate(blst)
+endif()
+
+SET(BLST_LIB_FILE ${blst_SOURCE_DIR}/libblast.a)
+add_custom_command(
+    OUTPUT ${BLST_LIB_FILE} 
+    COMMAND ./build.sh 
+    WORKING_DIRECTORY ${blst_SOURCE_DIR}
+)
+add_custom_target(blst_lib DEPENDS ${BLST_LIB_FILE})
